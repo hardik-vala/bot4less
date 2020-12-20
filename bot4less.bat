17e4178bb2f4d9b1c7a63aaefc6235c734973b57
Hardik Vala <hardikvala24@gmail.com>
2020-12-20 18:30:38 -0500

    Update bot4less.bat with password file

--- bot4less.bat
+++ bot4less.bat
@@ -1,2 +1,2 @@
 @echo off
-python C:\Users\Reservation-Spare\bot4less\python\bot4less.py --num_future_booking_days=2 >> C:\Users\Reservation-Spare\bot4less\logs.txt 2>&1
+python C:\Users\Reservation-Spare\bot4less\python\bot4less.py --password_path=C:\Users\Reservation-Spare\bot4less\fit4less_password.txt --num_future_booking_days=2 >> C:\Users\Reservation-Spare\bot4less\logs.txt 2>&1
